3253b1c870ee3cfdfe82b2d6192a90f19dabd5d6
Abdullah Khan <abdullahabdukhan@gmail.com>
2024-12-09 21:59:01 +0530

    gg

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
-
-dotenv.config()
-
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'import.meta.env.VITE_GROQ_API_KEY': JSON.stringify(process.env.VITE_GROQ_API_KEY)
-  }
 })
